1ed015d6b4cf304982b4938ecb0f87a28b193489
[Nikion] <[nigutjim@gmail.com]>
2022-05-13 11:27:32 -0500

    agregar el link para ir al home desde /admid/usuarios

--- pages/admin/usuarios/index.tsx
+++ pages/admin/usuarios/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Link from 'next/link'
 
 const index = () => {
   return (
-    <div>
-        Pagina de admin de usuarios
-    </div>
+      <div>
+        <div>Pagina de admin de usuarios</div>
+        <Link href='/'>
+            <a> Ir al home</a>
+        </Link>
+
+      </div>
   )
 }
 
