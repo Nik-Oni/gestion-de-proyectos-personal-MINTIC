1be8f1c34c86a93465498270986f1c9899a4a3ce
[Nikion] <[nigutjim@gmail.com]>
2022-05-14 09:51:35 -0500

    headers a las paginas y fontawesome

--- pages/admin/usuarios/index.tsx
+++ pages/admin/usuarios/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
 
 const index = () => {
   return (
       <div>
-        <div>Pagina de admin de usuarios con dev</div>
+        <Head>
+          <title>Admin usuarios | Gestion de Proyectos</title>
+        </Head>
+        <div>Pagina de admin de usuarios</div>
         <Link href='/'>
             <a> Ir al home</a>
         </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
     <div className='bg-red-500'>
+      <Head>
+        <title>Home</title>
+      </Head>
       <p>
         Pagina de Index
       </p>
